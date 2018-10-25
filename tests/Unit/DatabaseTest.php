6a758174496cd6b1787c125017e46305eb5f143d
Siddharth Santhanakrishnan <ss3875@njit.edu>
2018-10-25 19:19:38 -0400

    Feature: This unit test is to check if a specific data is present in the users database

---
+++ tests/Unit/DatabaseTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class DatabaseTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testDatabase()
+    {
+        $this->assertDatabaseHas('users', [
+           'email' => 'herman42@example.org'
+        ]);
+    }
+}
